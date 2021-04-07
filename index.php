f63db68319e9582591cd3edd42dd5cc38a485f1f
cristofer <jaimez07788@gmail.com>
2021-04-07 10:55:29 -0500

    modulos nuevos

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema PHP</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+    include_once 'controllers/Index.controller.php';
+    $controlador = new IndexController();
+
+
+    //peticion GET para pasar parametroas al controlador
+    if(!isset($_REQUEST['v'])){
+        $controlador->index();
+    }else{
+        $action = $_REQUEST['v'];
+        call_user_func(array($controlador, $action));
+    }
+?>
+
